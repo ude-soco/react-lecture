5488ae180f7405360ed3d29a887d99df7931dabb
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-10-03 12:17:17 +0200

    Add component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-// ****************************************
-// * Class based component (deprecated)
-// ****************************************
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       count: 7,
-//     };
-//   }
-//   render() {
-//     return <h1>{this.state.count}</h1>;
-//   }
-// }
+function App() {
+  // ****************************************
+  // ** Create a state using useState hook **
+  // ****************************************
+  const [name, setName] = useState("Simple Counter");
+  const [count, setCount] = useState(0);
+  // ****************************************
 
-// ****************************************
-// * Functional based component
-// ****************************************
-function App(props) {
-  const [count, setCount] = useState(7);
-  return <h1>{count}</h1>;
+  return (
+    <>
+      {/* // **** Displays the counter name ****** */}
+      <h4>{name}</h4>
+
+      {/* // ***** Displays the count value ****** */}
+      <h1>{count}</h1>
+
+       {/* // ***** Button to decrease count ****** */}
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+
+      {/* // ***** Button to increase count ****** */}
+      <button onClick={() => setCount(count - 1)}>
+        Decrease
+      </button>
+
+      {/* // * Button to reset the count to zero ** */}
+      <button onClick={() => setCount(0)}>
+        Reset
+      </button>
+
+      {/* // * Input field to rename the counter ** */}
+      <input onChange={(event) => setName(event.target.value)}/>
+    </>
+  );
 }
 
 export default App;
