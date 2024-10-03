81263a523eb0e3aeb924b7fbef7819e7eb5ebe92
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-10-03 14:01:10 +0200

    Add return statement back

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,36 @@ function App() {
   );
   // *********************************************
 
-  return <></>;
+  return (
+    <>
+      {/* // ** Displays the counter name ************* */}
+      <h4>{name}</h4>
+
+      {/* // TODO: Comment line 33 and uncomment line 36 below */}
+      {/* {nameHTML} */}
+
+      {/* // ** Displays the count value ************** */}
+      <h1>{count}</h1>
+
+      {/* // ** Button to decrease count ************* */}
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+
+      {/* // ** Button to increase count ************** */}
+      <button onClick={() => setCount(count - 1)}>
+        Decrease
+      </button>
+
+      {/* // ** Button to reset the count to zero ***** */}
+      <button onClick={() => setCount(0)}>
+        Reset
+      </button>
+
+      {/* // ** Input field to rename the counter ****** */}
+      <input onChange={(event) => setName(event.target.value)} />
+    </>
+  );
 }
 
 export default App;
