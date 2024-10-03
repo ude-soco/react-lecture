9777752d05680fa86b70676bade907db5d8d6ac7
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-10-03 16:50:01 +0200

    Add comments

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@
 // ** Use JavaScript to manipulate DOM *********
 // *********************************************
 // ** Find node with h1 tag ********************
-const header = document.querySelector("h1");
+// const header = document.querySelector("h1");
 
-console.log(header);
+// console.log(header);
 
 // ** Create a new node with p tag *************
-const paragraph = document.createElement("p");
-paragraph.textContent = "React Lecture";
+// const paragraph = document.createElement("p");
+// paragraph.textContent = "React Lecture";
 
 // ** Append it to the document body ***********
-document.body.appendChild(paragraph);
+// document.body.appendChild(paragraph);
