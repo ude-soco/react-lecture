628aab9b405ee17cfd1095605924391db1298f1e
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-10-03 13:45:29 +0200

    Add data types

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,33 @@
 import { useState } from "react";
 
 function App() {
-  // ****************************************
-  // ** Create a state using useState hook **
-  // ****************************************
+  // *********************************************
+  // ** Create a state using useState hook *******
+  // *********************************************
+  // ** String type ******************************
   const [name, setName] = useState("Simple Counter");
+
+  // ** Number ***********************************
   const [count, setCount] = useState(0);
-  // ****************************************
 
-  return (
-    <>
-      {/* // **** Displays the counter name ****** */}
-      <h4>{name}</h4>
+  // ** Array ************************************
+  const [nameArray, setNameArray] = useState([
+    "Awesome counter",
+    "Extraordinary Counter",
+  ]);
 
-      {/* // ***** Displays the count value ****** */}
-      <h1>{count}</h1>
-
-       {/* // ***** Button to decrease count ****** */}
-      <button onClick={() => setCount(count + 1)}>
-        Increase
-      </button>
-
-      {/* // ***** Button to increase count ****** */}
-      <button onClick={() => setCount(count - 1)}>
-        Decrease
-      </button>
-
-      {/* // * Button to reset the count to zero ** */}
-      <button onClick={() => setCount(0)}>
-        Reset
-      </button>
-
-      {/* // * Input field to rename the counter ** */}
-      <input onChange={(event) => setName(event.target.value)}/>
-    </>
+  // ** Objects **********************************
+  const [nameObject, setNameObject] = useState({
+    primary: "Awesome counter",
+    secondary: "Extraordinary Counter",
+  });
+  // ** HTML/CSS *********************************
+  const [nameHTML, setNameHTML] = useState(
+    <h4>HTML Counter Name</h4>
   );
+  // *********************************************
+
+  return <></>;
 }
 
 export default App;
