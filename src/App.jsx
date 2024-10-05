bda4a2c7a5659bc802f41a627fa05103b22d8665
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-10-05 17:40:05 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function App() {
-  // ** Create a state using useState hook **
+  // ** Create a state using useState hook *******************
   const [groceryItem, setGroceryItem] = useState("apple");
   const [groceryList, setGroceryList] = useState([
     "banana",
@@ -10,32 +10,56 @@ function App() {
     "chia seeds",
   ]);
 
+  // ** Custom function to add an item ***********************
+  const handleAddItem = () => {
+    setGroceryList((prevState) => [...prevState, groceryItem]);
+  };
+
+  // ** Custom function with parameters to add an item *******
+  const handleAddFruit = (fruit) => {
+    setGroceryList((prevState) => [...prevState, fruit]);
+  };
+
   return (
     <>
-      <h2>Grocery List</h2>
+      <div>
+        <h2>Grocery List</h2>
 
-      <ul>
-        {groceryList.sort().map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+        <ul>
+          {groceryList.sort().map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
 
-      <p>Enter item</p>
+        <p>Enter item</p>
 
-      {/* // ** Enter name of a grocery item **** */}
-      <input
-        value={groceryItem}
-        onChange={(event) => setGroceryItem(event.target.value)}
-      />
+        {/* // ** Enter name of a grocery item **** */}
+        <input
+          value={groceryItem}
+          onChange={(event) => setGroceryItem(event.target.value)}
+        />
 
-      {/* // ** Button to add grocery item ******* */}
-      <button
-        onClick={() =>
-          setGroceryList((prevState) => [...prevState, groceryItem])
-        }
-      >
-        Add
-      </button>
+        {/* // ** Button to add grocery item ******* */}
+        <button
+          onClick={() =>
+            setGroceryList((prevState) => [...prevState, groceryItem])
+          }
+        >
+          Add
+        </button>
+
+        {/* // ** Add grocery item with a custom function ******* */}
+        <button onClick={handleAddItem}>Add</button>
+      </div>
+      <div style={{ paddingTop: 8 }}>
+        {/* // ** Button to add fruits ******* */}
+        <button onClick={() => handleAddFruit("apple")}>
+          Add orange
+        </button>
+        <button onClick={() => handleAddFruit("kiwi")}>
+          Add Kiwi
+        </button>
+      </div>
     </>
   );
 }
